a0cfa14121a1ddc175adc67f523b3e00f750a12e
Roberto Stocco <robi.stocco@gmail.com>
2025-03-20 14:44:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # Enable for Langtrace.
 # from dotenv import load_dotenv
 # load_dotenv()
-import warnings
 from datetime import datetime
 from textwrap import dedent
 
@@ -11,29 +10,6 @@ from crew import ResearchCrew
 # Must precede any llm module imports.
 # from langtrace_python_sdk import langtrace
 # langtrace.init(api_key=os.getenv("LANGTRACE_API_KEY"))
-
-
-# Save a reference to the original 'showwarning' function.
-original_showwarning = warnings.showwarning
-# Define a custom filter function.
-
-
-# To avoid Pydantic deprecation warnings, add before importing CrewAI tools:
-def custom_showwarning(message, category, filename, lineno, file=None, line=None):  # pylint: disable=R0913,R0917
-  """ Filter out pydantic warnings.
-  CrewAI depends on Pydantic v2 but the "tools" package uses v1 notations that
-  have been deprecated, and that triggers many warnings output in stdout.
-  We want to filter them out.
-  """
-  if '/pydantic/_internal/' in filename or 'crewai_tools/tools' in filename:
-    return
-  # Show all other warnings.
-  original_showwarning(message, category, filename, lineno, file, line)
-
-
-# Replace the warning display function.
-warnings.showwarning = custom_showwarning
-
 
 if __name__ == "__main__":
   topic = input(dedent(""">>> Topic to research: """))
